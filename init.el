2e3a352880c9ab1db6f0dd2e81d11199f2e74332
Gerard Vermeulen <gerard.vermeulen@neel.cnrs.fr>
2019-05-28 15:51:40 +0200

    Tweak ivy and ivy-posframe.
    * Play around with ivy-posframe falling back to plain ivy in case of
      collisions with exwm-mode window buffers.

--- init.el
+++ init.el
@@ -1457,6 +1457,7 @@ With one prefix arg, show only EXWM buffers. With two, show all buffers."
   :custom
   (ivy-case-fold-search-default 'auto)
   (ivy-count-format "(%d/%d) ")
+  (ivy-display-function 'ivy-posframe-display)
   (ivy-height 10)
   (ivy-use-ignore-default t)
   (ivy-use-virtual-buffers t)
@@ -1464,13 +1465,50 @@ With one prefix arg, show only EXWM buffers. With two, show all buffers."
                ("C-c C-r" . ivy-resume)
                ("C-x B" . ivy-switch-buffer-other-window)
                ("C-x b" . my-ivy-switch-buffer)))
-  :commands (ivy-mode
+  :commands (ivy-display-function-fallback
+             ivy-mode
              ivy-read
              ivy-switch-buffer
              ivy-thing-at-point)
   :init
   (ivy-mode)
   :delight ivy-mode " 𝝓")
+
+(use-package ivy-posframe
+  :after ivy
+  :preface
+  (when (getenv "EXWM")
+    (defun advice-skip-exwm-in-ivy-posframe-display (fun &rest args)
+      (if (cl-loop for window being the windows of (selected-frame)
+                   when (with-current-buffer (window-buffer window)
+                          (eq major-mode 'exwm-mode))
+                   return t
+                   finally return nil)
+          (apply #'ivy-display-function-fallback args)
+        (apply fun args))))
+  :custom
+  (ivy-posframe-border-width 2)
+  (ivy-posframe-parameters '((left-fringe . nil)
+                             (right-fringe . 0)))
+  (ivy-posframe-style 'frame-center)
+  (ivy-posframe-width 80)
+  :commands (ivy-posframe-enable)
+  :demand t
+  :config
+  (when (getenv "EXWM")
+    (advice-add 'ivy-posframe-display-at-frame-bottom-left
+                :around #'advice-skip-exwm-in-ivy-posframe-display)
+    (advice-add 'ivy-posframe-display-at-frame-bottom-window-center
+                :around #'advice-skip-exwm-in-ivy-posframe-display)
+    (advice-add 'ivy-posframe-display-at-frame-center
+                :around #'advice-skip-exwm-in-ivy-posframe-display)
+    (advice-add 'ivy-posframe-display-at-point
+                :around #'advice-skip-exwm-in-ivy-posframe-display)
+    (advice-add 'ivy-posframe-display-at-window-bottom-left
+                :around #'advice-skip-exwm-in-ivy-posframe-display)
+    (advice-add 'ivy-posframe-display-at-window-center
+                :around #'advice-skip-exwm-in-ivy-posframe-display))
+  (ivy-posframe-enable))
 
 (use-package ivy-prescient
   :after ivy
