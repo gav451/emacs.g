e8b115d9b746626ccb8bb39ab22653b04de8a337
Gerard Vermeulen <gerard.vermeulen@neel.cnrs.fr>
2020-09-08 12:18:16 +0200

    Tweak `faces`
    * Set "FiraGo" to `variable-pitch` family on macOS.

--- init.el
+++ init.el
@@ -1236,7 +1236,11 @@ Use this to unregister from the D-BUS.")
   :init
   (set-face-attribute 'default nil :family "Hack" :height 110)
   (set-face-attribute 'fixed-pitch nil :family "Hack")
-  (set-face-attribute 'variable-pitch nil :family "DejaVu Sans"))
+  (cond ((eq system-type 'darwin)
+         (set-face-attribute 'variable-pitch nil :family "FiraGo"))
+        ((eq system-type 'gnu/linux)
+         (set-face-attribute 'variable-pitch nil :family "DejaVu Sans"))
+        (t (set-face-attribute 'variable-pitch nil :family "DejaVu Sans"))))
 
 (use-package files
   :commands (executable-find
