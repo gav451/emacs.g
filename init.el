5b374ad78fd1a2e72de2eb4bd907ade0af83fd88
Gerard Vermeulen <gerard.vermeulen@neel.cnrs.fr>
2020-09-10 18:39:25 +0200

    Tweak `faces`
    * Use larger font sizes on macOS.

--- init.el
+++ init.el
@@ -1242,13 +1242,19 @@ Use this to unregister from the D-BUS.")
 
 (use-package faces
   :init
-  (set-face-attribute 'default nil :family "Hack" :height 110)
-  (set-face-attribute 'fixed-pitch nil :family "Hack")
-  (cond ((eq system-type 'darwin)
-         (set-face-attribute 'variable-pitch nil :family "FiraGo"))
-        ((eq system-type 'gnu/linux)
-         (set-face-attribute 'variable-pitch nil :family "Fira Sans"))
-        (t (set-face-attribute 'variable-pitch nil :family "DejaVu Sans"))))
+  (cond
+   ((eq system-type 'darwin)
+    (set-face-attribute 'default nil :family "Hack" :height 120)
+    (set-face-attribute 'fixed-pitch nil :family "Hack")
+    (set-face-attribute 'variable-pitch nil :family "FiraGo"))
+   ((eq system-type 'gnu/linux)
+    (set-face-attribute 'default nil :family "Hack" :height 110)
+    (set-face-attribute 'fixed-pitch nil :family "Hack")
+    (set-face-attribute 'variable-pitch nil :family "Fira Sans"))
+   (t
+    (set-face-attribute 'default nil :family "Hack" :height 110)
+    (set-face-attribute 'fixed-pitch nil :family "Hack")
+    (set-face-attribute 'variable-pitch nil :family "DejaVu Sans"))))
 
 (use-package files
   :commands (executable-find
