20f5df17d89140e48562fe6cbf0eb082df7afda8
Gerard Vermeulen <gerard.vermeulen@neel.cnrs.fr>
2020-10-20 19:20:45 +0200

    Tweak `faces`
    * Select `FiraGO` as variable pitch typeface on Linux too.

--- init.el
+++ init.el
@@ -1269,7 +1269,7 @@ Use this to unregister from the D-BUS.")
    ((eq system-type 'gnu/linux)
     (set-face-attribute 'default nil :family "Hack" :height 110)
     (set-face-attribute 'fixed-pitch nil :family "Hack")
-    (set-face-attribute 'variable-pitch nil :family "Fira Sans"))
+    (set-face-attribute 'variable-pitch nil :family "FiraGo"))
    (t
     (set-face-attribute 'default nil :family "Hack" :height 110)
     (set-face-attribute 'fixed-pitch nil :family "Hack")
