083c9ef3459ee01704ecade3b5220fa349d6d685
Gerard Vermeulen <gerard.vermeulen@neel.cnrs.fr>
2021-05-19 07:09:54 +0200

    Tweak `consult`
    * Use the default key-binding to `yank`.

--- init.el
+++ init.el
@@ -360,7 +360,6 @@
          ("C-x 5 b" . consult-buffer-other-frame)
          ("C-x r x" . consult-register)
          ("C-x r b" . consult-bookmark)
-         ("C-y" . consult-yank-from-kill-ring)
          ;; M-g bindings (goto-map)
          ("M-g g" . consult-goto-line)
          ("M-g M-g" . consult-goto-line)
